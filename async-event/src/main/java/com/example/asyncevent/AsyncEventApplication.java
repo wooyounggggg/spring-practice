3e20459aa582ef844437df15be73899350006dbd
wooyounggggg <souljit2@gmail.com>
2022-04-01 21:17:47 +0900

    init: initialize async-event based project

---
+++ async-event/src/main/java/com/example/asyncevent/AsyncEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncEventApplication.class, args);
+    }
+
+}
---
+++ async-event/src/test/java/com/example/asyncevent/AsyncEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asyncevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
